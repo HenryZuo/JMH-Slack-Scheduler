838d27a151f98b1932ca0a2896b99d97b1d48194
maggie <hcc375@nyu.edu>
2017-07-18 14:35:41 -0700

    remove env

--- schedulerBot/env.sh
+++
@@ -1,8 +0,0 @@
-export SLACK_BOT_TOKEN='xoxb-213317745776-9vs5QSAwPyG3YGannVIBEzHL';
-export PORT='5000';
-export APIAI_TOKEN='ee1e205e770f42379e96f79c4955c7ee';
-export SLACK_WEBHOOK_URL='https://hooks.slack.com/services/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';
-export CLIENT_ID='853833657314-ti6ncseh876meg6d3rni4s8ec925otjg.apps.googleusercontent.com';
-export CLIENT_SECRET='yRszs7C1fd9Sxro2HxOxPwFd';
-export REDIRECT_URL='http://localhost:5000/google/callback'
-export MONGODB_URI='mongodb://maggiechu26:asdf1234@ds141209.mlab.com:41209/slack-scheduler-google-calendar'
